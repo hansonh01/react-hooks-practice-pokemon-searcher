0a6a5ce706b34ac29f60ce9f2289a2538c8ba7f2
Hanson <hanson.hua903@gmail.com>
2023-09-07 18:35:22 -0500

    Complete

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokeDex, setPokeDex] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/pokemon')
+      .then(r=>r.json())
+      .then(setPokeDex);
+  },[])
+
+  const handleAddPokemon = (newPokemon) => setPokeDex([...pokeDex, newPokemon]);
+
+  const filterPokeDex = pokeDex.filter((pokemon)=>{
+    return pokemon.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm
+        onAdd={handleAddPokemon}
+      />
       <br />
-      <Search />
+      <Search
+        search={search}
+        onSearch={setSearch}
+      />
       <br />
-      <PokemonCollection />
+      <PokemonCollection
+        pokeDex={filterPokeDex}
+      />
     </Container>
   );
 }
